5d60d8f09cec3b6827d0e95fa37fa50a78c00e95
Maksym <140002546+Machokar@users.noreply.github.com>
2024-03-14 19:26:13 +0000

    up

--- src/pages/Contacts/ContactsPage.jsx
+++ src/pages/Contacts/ContactsPage.jsx
@@ -3,7 +3,6 @@ import { SearchBox } from '../../components/SearchBox/Search';
 import { fetchContact } from '../../redux/operations';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import css from './ContactsPage.module.css';
 import { ContactForm } from '../../components/ContactForm/ContactFormу';
 
 const ContactsPage = () => {
@@ -14,8 +13,8 @@ const ContactsPage = () => {
   }, [dispatch]);
 
   return (
-    <div className={css.wrapper}>
-      <h1 className={css.text}>Phone Book</h1>
+    <div>
+      <h1>Phone Book</h1>
       <ContactForm />
       <SearchBox />
       <ContactList />
