de19d84aed62c5892cb288ffe835070129e6b15d
Maksym <140002546+Machokar@users.noreply.github.com>
2024-03-14 15:00:21 +0000

    Update register.jsx

--- src/pages/Register/register.jsx
+++ src/pages/Register/register.jsx
@@ -1,19 +1,10 @@
-import { useDispatch } from 'react-redux';
-import { register } from '../../redux/auth/operation';
 import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
 
 export default function RegisterPage() {
-  const dispatch = useDispatch();
-
-  const handleRegister = formData => {
-    const { credentials } = formData;
-    dispatch(register({ credentials }));
-  };
-
   return (
     <div>
-      <h1>Registration</h1>
-      <RegisterForm onSubmit={handleRegister} />
+      <p>SignUp </p>
+      <RegisterForm />
     </div>
   );
 }
